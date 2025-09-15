6192fcd9ac2921f89902f32e6522a0649fc0b039
maybar1 <x.may.bar@gmail.com>
2025-09-15 19:33:52 +0300

    DemoCssGrid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import DemoCssGrid from "./DemoCssGrid";
+import PureCssGrid from "./PureCssGrid";
 
 export default function App() {
   return (
     <div style={{ padding: 20 }}>
       <h1 style={{ textAlign: "center" }}>CSS Grid + Material UI Grid</h1>
 
-      {/* דוגמת Material UI Grid – 3 כרטיסים רספונסיביים */}
+      {/* Material UI Grid – 3 responsive cards */}
       <DemoCssGrid />
+
+      {/* Pure CSS Grid – auto-fit + minmax */}
+      <PureCssGrid />
     </div>
   );
 }
